56fb27edb84314cd9064110d94dafaa492d44fea
JinusMk <jinus@designstring.com>
2020-09-12 00:20:56 +0530

    added multiple dance forms

--- src/features/home/components/DanceFormCard.js
+++ src/features/home/components/DanceFormCard.js
@@ -1,30 +1,34 @@
 import React from 'react'
 
 export default function DanceFormCard(props){
+    const { dance } = props
     return(
         <div className="card">
                 <div className="top-blk">
-                    <img src={require('../../../assets/images/zumba_logo.svg')} className="logo" />
+                    <img src={dance.img} className="logo" />
                     <span className="secondaryText">Live & interactive</span>
-                    <p className="heading2">Zumba</p>
+                    <p className="heading2">{dance.title}</p>
                 </div>
                 <div className="info-blk">
                     <h3 className="heading3">
                         <img src={require('../../../assets/images/star_icon.svg')} />
-                        <span>4.5</span>
-                        <span className="rating">(89 ratings)</span>
+                        <span>{dance.rating}</span>
+                        <span className="rating">({dance.ratingCount} RATINGS)</span>
                     </h3>
-                    <p className="heading3 cost"><span>₹199</span>₹99</p>
-                    <p className="subHeading">1 hour class by Angel Bensy</p>
+                    <p className="heading3 cost"><span>{dance.costOld}</span>{dance.cost}</p>
+                    <p className="subHeading">{`${dance.duration} class by ${dance.instructor}`}</p>
                     <ul className="listInline">
-                        <li>
-                            <a className="primaryBtn round">10:00 AM</a>
-                            <p className="alert_red">ALMOST FULL</p>
-                        </li>
-                        <li>
-                            <a className="primaryBtn round">6:00 PM</a>
-                            <p className="alert_orange">FAST FILLING</p>
-                        </li>
+                        {
+                            dance.slots.map((item, index) => (index < (dance.slots.length <= 3 ? 3 : 2)) &&  <li key={index}>
+                                <a className="primaryBtn round">{item.time}</a>
+                                <p className="alert_red">{item.status}</p>
+                            </li>)
+                        }
+                        {
+                            dance.slots.length > 3 && <li key={2}>
+                                <a className="secondaryBtn round">+{dance.slots.length - 2} MORE</a>
+                            </li>
+                        }
                     </ul>
                 </div>
             </div>
--- src/features/home/components/UpComingClasses.js
+++ src/features/home/components/UpComingClasses.js
@@ -1,10 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Carousel from "react-multi-carousel";
 import { responsiveCarousel } from '../../../constants'
 import { DanceFormCard } from './index'
 import "react-multi-carousel/lib/styles.css";
 
 export default function UpComingClasses(props){
+    const [classes, setClasses] = useState([
+        {id: 1, img: require('../../../assets/images/zumba_logo.svg'), title: 'Zumba', rating: 4.5, ratingCount: 89, cost: '₹199', costOld: '₹99', instructor: 'Angel Bensy', duration: '1 hour', slots:[{time: '10:00 AM', status: 'ALMOST FULL'}, {time: '6:00 PM', status: 'FAST FILLING'}]},
+        {id: 2, img: require('../../../assets/images/bollywood_logo.svg'), title: 'Bollywood', rating: 4.5, ratingCount: 89, cost: '₹149', costOld: '₹249', instructor: 'Manas', duration: '1 hour', slots:[{time: '10:00 AM', status: 'ALMOST FULL'}, {time: '3:00 PM', status: 'FAST FILLING'}, {time: '6:00 PM', status: 'FAST FILLING'}, {time: '9:00 PM', status: ''}]},
+        {id: 3, img: require('../../../assets/images/zumba_logo.svg'), title: 'Zumba', rating: 4.5, ratingCount: 89, cost: '₹199', costOld: '₹99', instructor: 'Angel Bensy', duration: '1 hour', slots:[{time: '10:00 AM', status: 'ALMOST FULL'}, {time: '6:00 PM', status: 'FAST FILLING'}, {time: '9:00 PM', status: ''}]}])
     return(
         <div className="upcoming-classes-blk">
             <h3 className="heading2 title">Upcoming dances <a className="see-all paragraph"><span>See all </span><img src={require('../../../assets/images/right_arrow_icon.svg')} /></a></h3>
@@ -21,10 +25,7 @@ export default function UpComingClasses(props){
                 itemClass="carousel-item"
                 renderDotsOutside={true}
             >
-                
-                <DanceFormCard />
-                <DanceFormCard type="bollywood"/>
-                <DanceFormCard />
+                {classes.map((item, index) => <DanceFormCard key={index} dance={item}/>)}
             </Carousel>
         </div>
     )
