d086f825c42ba829a969d85834d26b6f817c1426
JinusMk <jinus@designstring.com>
2020-09-19 22:49:51 +0530

    minor update

--- src/features/schedule/containers/Schedule.js
+++ src/features/schedule/containers/Schedule.js
@@ -1,4 +1,4 @@
-import React, { Suspense, lazy, useState, useEffect } from 'react'
+import React, { Suspense, lazy, useState, useEffect, useRef } from 'react'
 import { Container } from '@material-ui/core';
 import { DanceInformationCard } from '../../../shared_elements'
 import { Tabs, Tab } from '@material-ui/core'
@@ -18,6 +18,7 @@ export default function Schedule(props){
             [moment().add(6, 'day')] : [{id: 1, rating: 4.5, img: require('../../../assets/images/bollywood_logo_card.svg'), title: 'Bollywood',ratingCount: 89, costOld: '₹199', cost: '₹99', instructor: { name: 'Angel Bensy', img: require('../../../assets/images/instructor_1.svg'), ratingCount: 89, rating: 4.5, expert: 'Zumba expert', experience: '5 years', classes: '51' },duration: '1 hour', participants: '86', morning: [{time: '6.00 AM', status: 'FAST FILLING'}, {time: '12.00 PM', status: 'Disabled'}], evening: [{time: '4.00 PM', status: 'FAST FILLING'}, {time: '8.00 PM', status: 'Disabled'}]}, {id: 2, rating: 4.5,title: 'Zumba', img: require('../../../assets/images/zumba_logo_card.svg'), ratingCount: 89, costOld: '₹199', cost: '₹99', instructor: { name: 'Angel Bensy', img: require('../../../assets/images/instructor_1.svg'), ratingCount: 89, rating: 4.5, expert: 'Zumba expert', experience: '5 years', classes: '51' },duration: '1 hour', participants: '86', morning: [{time: '6.00 AM', status: 'FAST FILLING'}, {time: '12.00 PM', status: 'Disabled'}], evening: [{time: '4.00 PM', status: 'FAST FILLING'}, {time: '8.00 PM', status: 'Disabled'}]}, {id: 3, title: 'HipHop', rating: 4.5, img: require('../../../assets/images/bollywood_logo_card.svg'), ratingCount: 89, costOld: '₹199', cost: '₹99', instructor: { name: 'Angel Bensy', img: require('../../../assets/images/instructor_1.svg'), ratingCount: 89, rating: 4.5, expert: 'Zumba expert', experience: '5 years', classes: '51' },duration: '1 hour', participants: '86', date: moment(), morning: [{time: '6.00 AM', status: 'FAST FILLING'}, {time: '12.00 PM', status: 'Disabled'}], evening: [{time: '4.00 PM', status: 'FAST FILLING'}, {time: '8.00 PM', status: 'Disabled'}]}],
         }
     )
+    const scrolllist = useRef(null);
     const handleChange = (event, newValue) => {
         setValue(newValue);
     };
@@ -28,6 +29,14 @@ export default function Schedule(props){
             behavior: 'smooth'
         });
     }, [])
+    const activeItem = () => {
+        // const indexActive = scrolllist.current.state.inViewState.findIndex(
+        //   (x) => x === true
+        // );
+        // console.log(scrolllist.current.props.items[indexActive]);
+      };
+      activeItem()
+      console.log(scrolllist);
     return(
         <section className="schedule-section">
             <Container className="schedule-container">
@@ -50,12 +59,14 @@ export default function Schedule(props){
                                         activeNavClass="is-current"
                                         headerBackground="true"
                                         scrollDuration={500}
-                                        onChange={(...params) => console.log(params)}
+                                        ref={scrolllist}
                                     >
                                      <ul>
-                                        {Object.keys(scheduleData).map((item, index) => <li><a href={`#date-${index+1}`} onChange={e => console.log('e.target', e.target)}><Tab key={index} label={<div className="date-item">
-                                            <h3 className="heading1">{moment(item).format('D') < 10 ? `0${moment(item).format('D')}` : moment(item).format('D')}</h3>
-                                            <p className="secondaryText">{moment(item).format('ddd')}</p>
+                                        {Object.keys(scheduleData).map((item, index) => <li><a href={`#date-${index+1}`} onChange={e => console.log('e.target', e.target)}>
+                                            <h3 className="heading3 month">{'September'}</h3>
+                                            <Tab key={index} label={<div className="date-item">
+                                                <h3 className="heading1">{moment(item).format('D') < 10 ? `0${moment(item).format('D')}` : moment(item).format('D')}</h3>
+                                                <p className="secondaryText">{moment(item).format('ddd')}</p>
                                         </div>}/></a> </li>)}
                                     </ul>
                                 </ScrollspyNav>
