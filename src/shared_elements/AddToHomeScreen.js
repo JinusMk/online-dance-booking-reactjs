75a3eb08b829336b3e2067bc1f5d473642e345c5
JinusMk <jinus@designstring.com>
2020-10-22 18:05:44 +0530

    add to home screen init

--- src/shared_elements/AddToHomeScreen.js
+++ src/shared_elements/AddToHomeScreen.js
@@ -1,8 +1,31 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 export default function AddToHomeScreen(props){
-    const addToHomeScreen = () => {
+    const [supportsPWA, setSupportsPWA] = useState(false);
+    const [promptInstall, setPromptInstall] = useState(null);
+
+    useEffect(() => {
+        const handler = e => {
+          e.preventDefault();
+          console.log("we are being triggered :D");
+          setSupportsPWA(true);
+          setPromptInstall(e);
+        };
+        window.addEventListener("beforeinstallprompt", handler);
+    
+        return () => window.removeEventListener("transitionend", handler);
+      }, []);
+
+    const addToHomeScreen = (evt) => {
         console.log('addToHomeScreen clicked')
+        evt.preventDefault();
+        if (!promptInstall) {
+            return;
+        }
+        promptInstall.prompt();
+    }
+    if (!supportsPWA) {
+        return null;
     }
     return(
         <div className="add-to-home-screen-blk">
