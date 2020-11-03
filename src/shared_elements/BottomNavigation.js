799e03f02ff7fafebc2e2b0fa6501dfaef307fd6
JinusMk <jinus@designstring.com>
2020-11-03 14:46:12 +0530

    updted the bottom navigation

--- src/shared_elements/BottomNavigation.js
+++ src/shared_elements/BottomNavigation.js
@@ -1,9 +1,10 @@
 import React, { useEffect } from 'react'
 import { Hidden, BottomNavigation, BottomNavigationAction } from '@material-ui/core'
 import { useHistory, useLocation } from "react-router-dom";
+import { connect } from 'react-redux'
 import { imageBasePath } from '../constants';
 
-export default function BottomNavigationComponent(props){
+function BottomNavigationComponent(props){
     const [value, setValue] = React.useState(null);
     let history = useHistory();
     let location = useLocation();
@@ -30,8 +31,13 @@ export default function BottomNavigationComponent(props){
                 >
                     <BottomNavigationAction onClick={() => { setValue(0); navigateTo("/");}} label="HOME" icon={(location.pathname === '/' || location.pathname == '/home') ? <img src={`${imageBasePath}home_icon_active.svg`} /> : <img src={`${imageBasePath}home_icon.svg`} />}/>
                     <BottomNavigationAction onClick={() => {setValue(1); navigateTo("/schedule")}} label="SCHEDULE" icon={location.pathname === '/schedule' ? <img src={`${imageBasePath}schedule_icon_active.svg`} /> : <img src={`${imageBasePath}schedule_icon.svg`} />}/>
-                    <BottomNavigationAction onClick={() => {setValue(2); navigateTo("/profile")}} label="YOU" icon={location.pathname === '/profile' ? <img src={`${imageBasePath}profile_icon_active.svg`} /> : <img src={`${imageBasePath}profile_icon.svg`} />}/>
+                    <BottomNavigationAction onClick={() => {setValue(2); navigateTo("/profile")}} label="YOU" icon={(props.isLoggedIn && props.userInfo.photoURL) ? <img src={props.userInfo.photoURL} className={`user-avatar ${location.pathname === '/profile' ? 'active': ''}`}/> : location.pathname === '/profile' ?  <img src={`${imageBasePath}profile_icon_active.svg`} /> : <img src={`${imageBasePath}profile_icon.svg`} />}/>
             </BottomNavigation>
         </Hidden>
     )
 }
+const mapStateToProps = state => ({
+    isLoggedIn: state.sharedReducers.isLoggedIn,
+    userInfo: state.sharedReducers.userInfo
+})
+export default connect(mapStateToProps)(BottomNavigationComponent)
