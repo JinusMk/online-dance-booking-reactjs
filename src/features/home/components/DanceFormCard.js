d70c2efaa4e9adfa380eccf22bfd50c8ec87c668
JinusMk <jinus@designstring.com>
2021-06-27 14:16:39 +0530

    added timeslots in the home page

--- src/features/home/components/DanceFormCard.js
+++ src/features/home/components/DanceFormCard.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import { useHistory } from 'react-router-dom'
 import Skeleton from '@material-ui/lab/Skeleton';
 import { isMobile } from 'react-device-detect'
+import moment from 'moment'
 import { imageBasePath, currencySymbol } from '../../../constants';
 
 export default function DanceFormCard(props){
@@ -28,18 +29,19 @@ export default function DanceFormCard(props){
                     <p className="subHeading"><img src={`${imageBasePath}clock_icon.svg`} /> <span>{`${dance.duration ? dance.duration : '1 hour'} class by ${dance.instructor && dance.instructor?.name ? dance.instructor?.name: '--'}`}</span></p>
                     <p className="subHeading description"><svg class="MuiSvgIcon-root jss67" focusable="false" viewBox="0 0 24 24" aria-hidden="true" tabindex="-1" title="DescriptionOutlined" data-ga-event-category="material-icons" data-ga-event-action="click" data-ga-event-label="DescriptionOutlined"><path d="M8 16h8v2H8zm0-4h8v2H8zm6-10H6c-1.1 0-2 .9-2 2v16c0 1.1.89 2 1.99 2H18c1.1 0 2-.9 2-2V8l-6-6zm4 18H6V4h7v5h5v11z"></path></svg> <span>{dance.description}</span></p>
                     <ul className="listInline">
-                        {/* 
-                            dance.buttons.map((item, index) => (index < (dance.buttons.length <= 3 ? 3 : 2)) &&  <li key={index}>
-                                <a className="primaryBtn round">{item}</a>
-                             <p className={item.status == "ALMOST FULL" ? "alert_red" : 'alert_orange'}>{item.status}</p>
-                            </li>)
-                            dance.buttons.length > 3 && <li key={2}>
+                        {
+                            dance.timeSlots && dance.timeSlots.length ? dance.timeSlots.map((time, index) => (index < (dance.timeSlots.length <= 3 ? 3 : 2)) &&  <li key={index}>
+                                <a className="primaryBtn round">{moment(time).format(`hh:mm A`)}</a>
+                             {/* <p className={item.status == "ALMOST FULL" ? "alert_red" : 'alert_orange'}>{item.status}</p> */}
+                            </li>) : null
+                        }
+                        {
+                            dance.timeSlots.length > 3 ? <li key={2}>
                                 <a className="secondaryBtn round">+{dance.buttons.length - 2} MORE</a>
-                            </li>
-                        
-                        */}
-                        <li style={{width: '100%', padding: 0, margin: 0}}><a className="secondaryBtn round" style={{borderRadius: 4}}>{`BOOK ${dance?.name?.toUpperCase()} CLASS`}</a></li>
+                            </li> : null
+                        }
                     </ul>
+                    {/* <p style={{width: '100%', padding: 0, margin: 0}}><a className="secondaryBtn round" style={{borderRadius: 4}}>{`BOOK ${dance?.name?.toUpperCase()} CLASS`}</a></p> */}
                 </div>
             </div>
     )
